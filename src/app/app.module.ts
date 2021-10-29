e378f85d2f9e37859ea88b5c1a17f896e3f11564
unknown <simalakama02020@gmail.com>
2021-10-29 16:52:56 +0700

    202102278 - mid - vincentzw

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,19 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { LeaderbordComponent } from './leaderbord/leaderbord.component';
+import { TournamentComponent } from './tournament/tournament.component';
+import { ProfileComponent } from './profile/profile.component';
 
-import { AppComponent } from './app.component';
+const routes: Routes = [
+  {path:'home',component:HomeComponent},
+  {path:'leaderborad',component:LeaderbordComponent},
+  {path:'tournament',component:TournamentComponent},
+  {path:'profile',component:ProfileComponent},
+];
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
 })
-export class AppModule { }
+export class AppRoutingModule { }
